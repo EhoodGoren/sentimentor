5e92de6655c7eea5f6826f365196a8f0e9c1e74b
EhoodGoren <ehoodgoren@gmail.com>
2021-09-20 18:15:30 +0300

    Added function that checks the polarity of a sentence
    Uses sentim-api.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const link = "https://sentim-api.herokuapp.com/";
+
+async function getSentim(text="") {
+    const response = await fetch ("https://sentim-api.herokuapp.com/api/v1/", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"text": `${text}`})
+    });
+    const result = await response.json();
+    return result;
+}
+getSentim();
