1aa798f4afb0f1e58954a7712e9cbe30f7f70fbd
EhoodGoren <ehoodgoren@gmail.com>
2021-09-20 19:02:38 +0300

    Result section now shows the polarity and sentim type of the text sent.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const link = "https://sentim-api.herokuapp.com/";
-
 async function getSentim(text="") {
     const response = await fetch ("https://sentim-api.herokuapp.com/api/v1/", {
         method: "POST",
@@ -12,4 +10,35 @@ async function getSentim(text="") {
     const result = await response.json();
     return result;
 }
-getSentim();
+
+async function sendText(){
+    const textToSend = document.querySelector("#text-area").value;
+    const sentim = await getSentim(textToSend);
+
+    const textSentimColor = getColor(sentim.result.polarity);
+
+    const resultDiv = document.createElement("div");
+
+    resultDiv.innerText = 
+        `Your Sentence: ${sentim.sentences[0].sentence}
+        Polarity: ${sentim.result.polarity}
+        Type: ${sentim.result.type}`
+    resultDiv.style.color = textSentimColor;
+
+    document.querySelector("#result").appendChild(resultDiv);
+}
+
+function getColor(sentim) {
+    if (sentim < 0){
+        return "rgb(255,0,0)";
+    }
+    if (sentim > 0){
+        return "rgb(0,255,0)";
+    }
+    else {
+        return "rgb(90,90,90)";
+    }
+}
+
+const sendButton = document.querySelector("#send-button");
+sendButton.addEventListener("click", sendText);
